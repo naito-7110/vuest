1ce7ffd65bd8828cf8c8bc1cd1742821eecbe85b
naito-7110 <shige.7110.330@gmail.com>
2026-03-29 20:05:32 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Node 25 devShell";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+          pnpm
+          vscode-langservers-extracted
+          typescript-language-server
+        ];
+
+        shellHook = ''
+          echo "Node: $(node -v)"
+        '';
+      };
+    };
+}
