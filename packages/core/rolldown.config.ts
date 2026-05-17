d739587590ff559015616f2a3e1f0f2655f83345
naito-7110 <shige.7110.330@gmail.com>
2026-05-17 23:21:18 +0900

    refactor(@vuest/core): change import name Vue -> vue

--- packages/core/rolldown.config.ts
+++ packages/core/rolldown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'rolldown'
-import Vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   input: 'src/index.ts',
-  plugins: [Vue()],
+  plugins: [vue()],
 })
