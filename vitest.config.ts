7bdefbdd77af1bbfac9d7aa481a70e02dc0e78d0
naito-7110 <shige.7110.330@gmail.com>
2026-05-17 23:23:41 +0900

    test(root): add @vitejs/plugin-vue into viteset configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   test: {
     projects: [
       {
+        plugins: [vue()],
         test: {
           name: 'unit',
           environment: 'happy-dom',
